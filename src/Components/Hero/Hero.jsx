170e0dc4aec5657a7a260456657d72358980c1c5
Артемий Горлатов <artemijgorlatov@Noutbuk-Artemij.local>
2024-09-26 19:06:50 +0300

    first

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <div className="hero__wrapper width-restriction">
+            <div className="">
+                7tyruytliukjhg
+            </div>
+            <div className="">
+                7
+            </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
